d8169bc5925b679a7fca4a5bbf95601b2b03e0f2
Mahesh Chandra <mahesh.chandra@aeriestechnology.com>
2021-08-18 09:07:43 +0530

    read json files using python script

---
+++ python/files/read_json_file.py
@@ -0,0 +1,16 @@
+import json
+  
+# Opening JSON file
+f = open('data.json',)
+  
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+  
+# Iterating through the json
+# list
+for i in data['emp_details']:
+    print(i)
+  
+# Closing file
+f.close()
