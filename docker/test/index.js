bc4e08bf0ff5a0b21fcba799e9e8d93fac6ebb4e
Mahesh Chandra Maheswaram <mahesh.maheswaram@cloud4c.com>
2023-01-05 19:10:38 +0530

    updated docker example

--- docker/test/index.js
+++ docker/test/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function(req, res){
-res.json({'msg': 'hello world'})
+app.get('/', function (req, res) {
+	res.json({ 'msg': 'hello world compose2' })
 })
 
 
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, function(){
+app.listen(port, function () {
 	console.log('server started')
 })
